c75b75b2694f9371daa986d7bc304fb9561d2199
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-01-26 20:06:24 -0800

    finished about me

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -4,7 +4,19 @@ class AboutPage extends Component {
   render() {
     return (
       <div className="AboutPage">
-        <h1>This will be the About Page </h1>
+        <h1>About me</h1>
+        <p>I am a fullstack software engineer studying at Rithm School. </p>
+        <br />
+        <p>
+          Prior to transitioning to tech I was a music teacher and band
+          director. My love of organization, problem solving, and growth mindset
+          led me to study programming.{' '}
+        </p>
+        <br />
+        <p>
+          When not coding you can find me hiking with my family, building Lego,
+          or making gluten free banana pancakes.{' '}
+        </p>
       </div>
     );
   }
--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ContactButtons from './ContactButtons';
 
 class ContactPage extends Component {
   render() {
     return (
       <div className="ContactPage">
         <h1>This will be the Contact Page </h1>
+        <ContactButtons />
       </div>
     );
   }
