696bfea4f81b7e7f360e30ffbf630a6f763d9142
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-01-26 21:27:12 -0800

    added work links and screenshots

--- src/WorkPage.js
+++ src/WorkPage.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import matchingScreenshot from './matchingScreenshot.png';
+import joblyScreenshot from './joblyScreenshot.png';
 
 class WorkPage extends Component {
   render() {
     return (
-      <div className="WorkPage">
-        <h1>This will be the Work Page </h1>
+      <div className="container">
+        <div className="WorkPage">
+          <h1>This will be the Work Page </h1>
+          <div className="row">
+            <div className="col-lg-4 col-md-4 col-xs-4">
+              <a href="https://katiejessupmcd.github.io/matching_game/">
+                <img
+                  style={{ width: '300px' }}
+                  class="img-responsive"
+                  src={matchingScreenshot}
+                  alt="matchingScreenshot"
+                />
+              </a>
+            </div>
+            <div className="col-lg-4 col-md-4 col-xs-4">
+              <a href="https://react-jobly-frontend-katieg.herokuapp.com/">
+                <img
+                  style={{ width: '300px' }}
+                  class="img-responsive"
+                  src={joblyScreenshot}
+                  alt="joblyScreenshot"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
